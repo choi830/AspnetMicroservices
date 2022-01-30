9ae3838f7a0223837a22cae166ce36d9ac615e49
Andy Choi <choi830@hotmail.com>
2022-01-29 22:04:20 -0500

    1-29

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Ordering.Application.Features.Orders.Commands.CheckOutOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName).NotEmpty().WithMessage("{userName} is required.").NotNull().MaximumLength(50).WithMessage("{userName} is must not exceed 50 character.");
+
+            RuleFor(p => p.EmailAddress).NotEmpty().WithMessage("{EmailAddress} is required.");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} is required.")
+                .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero.");
+        }
+    }
+
+}
